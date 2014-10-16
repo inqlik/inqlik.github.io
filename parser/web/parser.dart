1705c6802c94e54b2f91f06c30acf4b76d4667c8
Vadim Tsushko <vadimtsushko@gmail.com>
2014-10-16 20:19:48 +0600

    Add online expression parser

---
+++ parser/web/parser.dart
@@ -0,0 +1,28 @@
+import 'dart:html';
+import 'package:qv_exp/src/parser.dart';
+import 'package:petitparser/petitparser.dart' as p;
+bool dirty = true;
+void main() {
+  querySelector("#parse_expression")
+      ..onClick.listen(parseExpression);
+  querySelector("#formula_text")..onInput.listen(invalidateResult);
+}
+
+void parseExpression(MouseEvent event) {
+  var expressionText = (querySelector("#formula_text") as TextAreaElement).value;
+  var qvs = new QvExpParser();
+  var parser = qvs['start'].end();
+  p.Result parseResult = new QvExpParser().guarded_parse(expressionText);
+  String  resultText = 'Expression parsed successfully';
+  if (parseResult.isFailure) {
+    resultText = 'Error while parsing expression: ${parseResult.message} at position ${parseResult.position}';
+  }
+  querySelector("#result").text = resultText;
+  dirty = false;
+}
+void invalidateResult(_) {
+  if (!dirty) {
+    querySelector("#result").text = '';
+    dirty = true;
+  }  
+}
